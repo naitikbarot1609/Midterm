adaee02032fab8ccf9110a60c8cbe753f82b1634
naitikbarot1609 <naitikbarot99@gmail.com>
2024-10-21 18:43:55 -0400

    [Code Updated]

--- test/test_app.py
+++ test/test_app.py
@@ -4,4 +4,4 @@ def test_hello():
     tester = app.test_client()
     response = tester.get('/')
     assert response.status_code == 200
-    assert b"Hello, Flask!" in response.data
+    assert b"Hello World" in response.data
